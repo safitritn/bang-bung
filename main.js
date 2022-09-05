6c44407f0a3bd534f2a874d64c37360a2a4492fa
safitritn <safitritina441@gmail.com>
2022-09-05 09:27:34 +0700

    new

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-let bohlam = document.getElementById('bohlam');
-for (let i = 0; i < 1000; i++) {
-let trafficLight = `<div class="traffic" id="traffic` + i + `">
-                <div class="light bulat"></div>
-                <button class="button bulat" data-index="`+ i + `"></button></div>`
+let container = document.getElementById('container');
+        for (let i = 0; i < 100; i++) {
+            let colorbohlam = ""
+            if (i % 3 == 0){
+                colorbohlam = "DarkRed";
+                
+            }else if (i % 2 == 0){
+                colorbohlam = "DarkGreen";
 
-bohlam.innerHTML += trafficLight
-}
+            }else  if(i % 1 == 0){
+                colorbohlam = "CanaryYellow";
+            }
+            
+            container.innerHTML += `<div class="bohlam" id="bohlam` + i + `">
+                <div class= "btnToggle circle ${colorbohlam}"></div>
+                <button class="button circle" data-index="`+ i + `"></button> </div>`
+            }
 
-function toggleLight(index) {
-let light = document.getElementById('traffic' + index).querySelector('.light');
-light.classList.toggle('on')
-}
+            function toggleLight(index) {
+                let beralih = document.getElementById('bohlam' + index).querySelector('.btnToggle');
+                beralih.classList.toggle('on')
+            }
 
-bohlam.addEventListener('click', function (event) { 
-    { let index = event.target.attributes['data-index'].value
-    toggleLight(index)}
-});
+
+
+
+            container.addEventListener('click', function (event) {
+                if (event.target.classList.contains('button')) {
+                    let index = event.target.attributes['data-index'].value
+                    toggleLight(index)
+                }
+            });
